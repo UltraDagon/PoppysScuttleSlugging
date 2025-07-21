623ed8cb16122756d62e4ab29af3f2bc8643ac3a
Dagon Williams <dagon.williams@gmail.com>
2025-07-21 02:00:23 -0500

    Documented InputHandler.h and Sprite.h

--- InputHandler.h
+++ InputHandler.h
@@ -9,20 +9,36 @@
 class InputHandler
 {
 public: // TODO: Make private
+  /// Pointer to the running variable that controls the gameloop.
   bool *running;
+  /// Pointer to the Renderer that controls the window rendering and camera.
   Renderer *renderer;
+  /// The event queue that contains all events that have occured after the previous frame.
   SDL_Event event;
-
+  /// Map of all keyboard keys (true == pressed, false == unpressed).
   std::unordered_map<int, bool> keysPressed;
+  /// X position of the mouse within the window.
   int mouseX;
+  /// Y position of the mouse within the window.
   int mouseY;
+  /// The state of the mouse buttons.
   Uint32 mouseState;
 
 public:
-  InputHandler(bool *_running, Renderer *_renderer); // Controls the running state of the program, the renderer,
+  /**
+   * Construct a new Input Handler object.
+   * 
+   * @param running_ the running variable that controls the gameloop
+   * @param renderer_ the Renderer that controls the window rendering and camera
+   */
+  InputHandler(bool *_running, Renderer *_renderer);
+  // Todo: flip underscores to the right ^, also maybe change these to references taken in at handleInput to save memory (not really needed)
 
   //~InputHandler();
 
+  /**
+   * Handle the input for the current frame and update InputHandler's variables.
+   */
   void handleInput();
 };
 
--- Sprite.h
+++ Sprite.h
@@ -1,6 +1,6 @@
 #include "ImageCache.h"
 
-#include <SDL2/SDL.h>
+#include <SDL2/SDL.h> // Todo: maybe this can be removed? I think imagecache has sdl2/sdl.h
 #include <string>
 
 #ifndef SPRITE_H
@@ -17,19 +17,43 @@ public: // make private
   int y;
   int width;
   int height;
+  /// The name of the image file that will be rendered from.
   std::string bmpAddress;
-
-  // SDL_Surface *surface;
+  /// The pointer of the texture used to render the Sprite.
   SDL_Texture *texture;
-  SDL_Rect frameRect; // Snippet of image to be rendered
+  /// The rect within the image to render to the screen. Must be smaller than the image dimensions.
+  SDL_Rect frameRect;
 
 public:
+// Phase this one out
   Sprite(int _x, int _y, int _width, int _height, std::string bmpAddress, ImageCache *images);
 
-  Sprite(int x_, int y_, int width_, int height_, std::pair<int, int> frameSize, std::pair<int, int> frameOffset, std::string bmpAddress, ImageCache *images);
+  /**
+   * Construct a new Sprite object.
+   * 
+   * @param x_ x position in the world
+   * @param y_ y position in the world
+   * @param width_ width of the sprite in the world
+   * @param height_ height of the sprite in the world
+   * @param frameSize size of the frame within the image file
+   * @param frameOffset offset of the frame from the top left of the image file
+   * @param bmpAddress_ name of the image file
+   * @param images the ImageCache that manages image files
+   */
+  Sprite(int x_, int y_, int width_, int height_, std::pair<int, int> frameSize, std::pair<int, int> frameOffset, std::string bmpAddress_, ImageCache *images);
 
+  /**
+   * Destroy the Sprite object.
+   */
   ~Sprite();
 
+  /**
+   * Get the SDL_Texture of the Sprite.
+   * 
+   * @param sdlRenderer the SDL_Renderer that the Sprite will be renderered on
+   * @param images the ImageCache that manages image files
+   * @return SDL_Texture*
+   */
   SDL_Texture *getTexture(SDL_Renderer *sdlRenderer, ImageCache *images);
 };
 
